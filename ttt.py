732bd2162b2fd814e362701138f6925aa43e7caf
Clifton Chiang <thechiangsta@gmail.com>
2021-04-03 22:38:46 -0700

    Changed the assigning of roles to be in line with the actual game. Made it more intuitive to add recipients.

--- ttt.py
+++ ttt.py
@@ -1,7 +1,11 @@
+# Author: Clifton Chiang
+# Date: 04/03/2021
+
 # Import libraries
 import email.utils
 import re
 import os
+import math
 import smtplib
 import random
 import getpass
@@ -9,63 +13,78 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
 if __name__ == '__main__':
+    senderEmail = '' # Sender's email address 
+    password = '' # App password 
+    recipients = {}
+    # Read the recipients file and extract the email,
+    # password, and recipients
+    with open('recipients.txt', 'rt') as file:
+        for line in file:
+            if(line.startswith('email')):
+                senderEmail = re.search('\"(\S+@\S+)\"', line)[0]
+            elif(line.startswith('password')):
+                password = re.search('\"(.+?)\"', line)[0]
+            elif(not line.startswith('//') and not line == '\n'):
+                matches = re.search('^([0-9]+@[A-Za-z.]+),\W*(\S+)$', line)
+                recipients[matches[2].strip()] = matches[1].strip()
+    
+        
+    # The rules of TTT state that generally the number of
+    # detectives, traitors, and innocents are percentages
+    # of the total number of players.
+    # https://trouble-in-terrorist-town.fandom.com/wiki/Detective#:~:text=The%20Detectives%20are%20the%20smallest,out%20who%20the%20Traitors%20are.
+    numRoles = len(recipients)
+    numDetectives = round(numRoles * 0.125)
+    numTraitors = round(numRoles * 0.25)
+    numInnocents = round(numRoles * 0.625)
+    
+    # Fill the roles array with correct number of detectives, traitors, and innocents
     # 0 - Detective
     # 1 - Traitor
     # 2 - Innocent
-    # Here you can modify the roles for the game
-    roles = [0, 1, 1, 2, 2, 2]
+    roles = []
+    for i in range(numDetectives):
+        roles.append(0)
+    for i in range(numTraitors):
+        roles.append(1)
+    for i in range(numInnocents):
+        roles.append(2)    
+
+    # Ensure the roles are randomized for the players
     random.shuffle(roles)
 
-    # Get the sender and recipient information
-    sender = "" # Put your email address here
-    file = open('phonenumbers.txt')
-    recipients = {}
-    for line in file:
-        recTemp = line.split(',')
-        # Ignore lines that start with //
-        if(not recTemp[0].startswith('//', 0, 2) and not recTemp[0] == '\n'):
-            number = recTemp[0].strip()
-            name = recTemp[1].strip()
-            recipients[name] = number
-
+    # Determine who is what role
     detective = [name for i, (name, phoneNum) in enumerate(recipients.items()) if roles[i] == 0]
     traitors = [name for i, (name, phoneNum) in enumerate(recipients.items()) if roles[i] == 1]
     innocents = [name for i, (name, phoneNum) in enumerate(recipients.items()) if roles[i] == 2]
-
-    generalMsg = ("\r\n%s is the detective!" % detective[0])
-
-    print('Detective:', detective)
-    print('Traitors:', traitors)
-    print('Innocents:', innocents)
-    # print(recipients)
+  
+    # The message that will be sent to everyone
+    generalMsg = ('\r\n%s is the detective!' % detective[0])
 
     # Start tls session
-    s = smtplib.SMTP("smtp.gmail.com", 587)
+    s = smtplib.SMTP('smtp.gmail.com', 587)
     s.starttls()
-    password = "" # App password goes here
-    s.login(sender, password)
+    s.login(senderEmail, password)
 
     # Detective
     recipient = recipients[detective[0]]
-    body = "\r\nYou are the detective!"
-    # print(body)
-    s.sendmail(sender, recipient, body)
+    body = '\r\nYou are the detective!'
+    s.sendmail(senderEmail, recipient, body)
 
     # Traitors
     for traitor in traitors:
         recipient = recipients[traitor]
-        body = generalMsg + "\r\nYou are a traitor! Here are your fellow traitors:\r\n" + \
+        body = generalMsg + '\r\nYou are a traitor! Here are your fellow traitors:\r\n' + \
             ', '.join([v for i, v in enumerate(traitors) if v != traitor])
-        # print(body)
-        s.sendmail(sender, recipient, body)
+        s.sendmail(senderEmail, recipient, body)
         
 
     # Innocents
     for innocent in innocents:
         recipient = recipients[innocent]
-        body = generalMsg + "\r\nYou are an innocent!"
-        # print(body)
-        s.sendmail(sender, recipient, body)
+        body = generalMsg + '\r\nYou are an innocent!'
+        s.sendmail(senderEmail, recipient, body)
 
     # End session
     s.quit()
+
